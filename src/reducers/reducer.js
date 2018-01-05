c5b9af0e0b46b8df502330c1c2a810f61bc5db99
Christian <ChrisWcs@users.noreply.github.com>
2018-01-05 14:39:49 -0700

    Finish reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -4,8 +4,28 @@ import initialState from './initialState';
 const reducer = (state = initialState(), action) => {
     switch(action.type){
         case ADD_TODO:
+            return {
+                todos: [...state.todos, {
+                    name: action.name,
+                    toggle: false,
+                }],
+                inputValue: "",
+            };
         case DELETE_TODO:
+            return {
+                ...state,
+                todos: [...state.todos.slice(0, action.index), ...state.todos(action.index + 1)],
+            };
         case TOGGLE_TODO:
+            return {
+                ...state,
+                todos: [...state.todos.slice(0, action.index), 
+                {
+                    name: state.todos[action.index].name,
+                    toggle: true,
+                }, 
+                ...state.todos(action.index + 1)],
+            };
         case CHANGE_INPUT:
             return {
                 ...state,
