3a4b066f284873cf437197e566a18dec73255922
Christian <ChrisWcs@users.noreply.github.com>
2018-01-09 15:04:18 -0700

    Finish Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import TodoTxt from '../styledcomps/TodoTxt';
 import TodoBtn from '../styledcomps/TodoBtn';
 import FlexRow from '../styledcomps/FlexRow';
 
-const Todo = () => (
+const Todo = ({todo, deleteFunc, toogleFunc}) => (
     <FlexRow>
-        <TodoBtn>Toogle</TodoBtn>
-        <TodoBtn>Delete</TodoBtn>
+        <TodoTxt>{todo}</TodoTxt>
+        <TodoBtn onClick={toogleFunc}>Toogle</TodoBtn>
+        <TodoBtn onClick={deleteFunc}>Delete</TodoBtn>
     </FlexRow>
 );
+
+Todo.propTypes = {
+    todo: PropTypes.string,
+    deleteFunc: PropTypes.func,
+    toogleFunc: PropTypes.func,
+};
 
 export default Todo;
