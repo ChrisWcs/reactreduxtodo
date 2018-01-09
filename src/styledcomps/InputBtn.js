e760da328e9a2ababce9151a7804773c2f340845
Christian <ChrisWcs@users.noreply.github.com>
2018-01-08 23:53:32 -0700

    Add styling to input component

--- src/styledcomps/InputBtn.js
+++ src/styledcomps/InputBtn.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 const InputBtn = styled.button`
-
-
+    font-family: Arial;
+    font-size: 25px;
+    background: white;
+    border: solid 1px black;
 `;
 
 export default InputBtn;
--- src/styledcomps/InputField.js
+++ src/styledcomps/InputField.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 const InputField = styled.input`
-
-
+    font-family: Arial;
+    font-size: 25px;
+    background: white;
+    border: solid 1px black;
+    letter-spacing: 1px;
 `;
 
 export default InputField;
