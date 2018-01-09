46884b4db582f05e004c3ade601ac0784320e69d
Christian <ChrisWcs@users.noreply.github.com>
2018-01-08 22:58:24 -0700

    Create ConInput container component

---
+++ src/containers/ConInput.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import Input from '../components/Input';
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    
+});
+
+
+const ConInput = connect(mapStateToProps, mapDispatchToProps)(Input);
+
+export default ConInput;
