fc8d30a24c20c212b18578baca8c50570f61cf53
Christian <ChrisWcs@users.noreply.github.com>
2018-01-09 15:10:32 -0700

    Implement Todo and Todos components

---
+++ src/components/Todos.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Todo from './Todo';
+
+import FlexCol from '../styledcomps/FlexCol';
+
+const Todos = ({ list, deleteFunc, toggleFunc}) => (
+    <FlexCol>
+        { list.map( (x, i) => <Todo key={i} todo={x} deleteFunc={deleteFunc(i)} toggleFunc={toggleFunc(i)}/>) }
+    </FlexCol>
+);
+
+Todos.propTypes = {
+    list: PropTypes.array,
+    deleteFunc: PropTypes.func,
+    toggleFunc: PropTypes.func,
+};
+
+export default Todos;
