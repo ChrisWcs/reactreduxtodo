861b74bbf71a116963172c8506004c2da75027ad
Christian <ChrisWcs@users.noreply.github.com>
2018-01-08 23:43:24 -0700

    Finish ConInput

--- src/containers/ConInput.js
+++ src/containers/ConInput.js
@@ -1,12 +1,19 @@
 import { connect } from 'react-redux';
+import { createAddtodo, createChangeInput } from '../actions/actionCreators';
 import Input from '../components/Input';
 
 const mapStateToProps = (state) => ({
-
+    value: state.inputValue
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    
+    handleValueFunc: (event) => {
+        let {value} = event.target;
+        dispatch(createChangeInput(value));
+    }, 
+    addFunc: () => {
+        dispatch(createAddtodo());
+    }
 });
 
 
